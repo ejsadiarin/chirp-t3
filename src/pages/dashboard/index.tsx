e532b2eb30656a75a6893de2eb09b622ac688d69
ejsadiarin <ejsadiarin@gmail.com>
2023-09-17 23:04:24 +0800

    Router

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,12 @@
+import Sidebar from "./Sidebar";
+
+export default function Dashboard() {
+  return (
+    <>
+      <div className="">
+        <h1>Dashboard</h1>
+        {/* <Sidebar /> */}
+      </div>
+    </>
+  );
+}
